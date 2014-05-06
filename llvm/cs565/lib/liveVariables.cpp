75c55782955d1d21e0cea7040d45311246c18dab
ljfranklin <lylejfranklin@gmail.com>
2014-05-05 20:53:01 -0700

    Finished first pass of live analysis

--- llvm/cs565/lib/liveVariables.cpp
+++ llvm/cs565/lib/liveVariables.cpp
@@ -7,13 +7,14 @@
 #include "llvm/IR/CFG.h"
 #include <map>
 #include <set>
+#include <algorithm>
 
 namespace cs565 {
 	bool LiveVariables::runOnFunction(Function &F) {
 		
 		errs() << F.getName() << "\n";
     
-    	//build USE/DEF sets for each BB
+    	//build USE + DEF sets for each BB
     	std::map<BasicBlock*, std::set<std::string>> useMap;
     	std::map<BasicBlock*, std::set<std::string>> defMap;
     	for (Function::iterator currentBlock = F.begin(), endBlock = F.end(); currentBlock != endBlock; currentBlock++) {
@@ -29,9 +30,10 @@ namespace cs565 {
 				}
 				
 				for(Instruction::op_iterator op = inst->op_begin(), ope = inst->op_end(); op != ope; op++) {
-					if(dyn_cast<Instruction>(*op) || dyn_cast<Argument>(*op)) {
-						Value* def = op->get();
-						use.insert(def->getName());
+					
+					if (dyn_cast<Instruction>(*op) || dyn_cast<Argument>(*op)) {
+						Value* define = op->get();
+						use.insert(define->getName());
 					}
 				}
     		}
@@ -39,15 +41,60 @@ namespace cs565 {
     		useMap[currentBlock] = use;
     		defMap[currentBlock] = def;
     	}
+    
+    	//Build IN + OUT sets
+    	std::map<BasicBlock*, std::set<std::string>> inMap;
+    	std::map<BasicBlock*, std::set<std::string>> outMap;
     	
+    	bool isChanged = true;
+    	
+    	while (isChanged) {
+    		isChanged = false;
+    		for (Function::iterator currentBlock = F.begin(), endBlock = F.end(); currentBlock != endBlock; currentBlock++) {
+			
+				int originalInCount = inMap[currentBlock].size();
+				int originalOutCount = outMap[currentBlock].size();
+			
+				for (succ_iterator iterator = succ_begin(currentBlock), endSucc = succ_end(currentBlock); iterator != endSucc; iterator++) {
+				  	BasicBlock *succ = *iterator;
+				  	
+				  	std::set<std::string> succIn = inMap[succ];
+				  	outMap[currentBlock].insert(succIn.begin(), succIn.end());
+				}
+			
+				std::set<std::string> inSet;
+			
+				std::set<std::string> diff;
+				std::set_difference(outMap[currentBlock].begin(), outMap[currentBlock].end(), 
+					defMap[currentBlock].begin(), defMap[currentBlock].end(), 
+					std::inserter(diff, diff.end()));
+			
+				inSet.insert(useMap[currentBlock].begin(), useMap[currentBlock].end());
+				inSet.insert(diff.begin(), diff.end());
+			
+				inMap[currentBlock] = inSet;
+				
+				int newInCount = inMap[currentBlock].size();
+				int newOutCount = outMap[currentBlock].size();
+				
+				if (newInCount != originalInCount || newOutCount != originalOutCount) {
+					isChanged = true;
+				}
+			}
+    	}
+    	
+		
+		
+		//Print sets
     	for (Function::iterator currentBlock = F.begin(), endBlock = F.end(); currentBlock != endBlock; currentBlock++) {
     			
-    		errs() << "\nBASIC BLOCK " << currentBlock->getName() << "\n";
+    		errs() << "\nBASIC BLOCK " << currentBlock->getName() << "\nInstructions:\n";
     		
     		for (BasicBlock::iterator inst = currentBlock->begin(), instEnd = currentBlock->end(); inst != instEnd; inst++) {
 				errs() << *inst << "\n";
     		}
     		
+    		/*
     		errs() << " USE \n";
     		for (std::set<std::string>::iterator use = useMap[currentBlock].begin(), useEnd = useMap[currentBlock].end(); use != useEnd; use++) {
 				errs() << "\t" << *use << "\n";
@@ -57,21 +104,16 @@ namespace cs565 {
     		for (std::set<std::string>::iterator def = defMap[currentBlock].begin(), defEnd = defMap[currentBlock].end(); def != defEnd; def++) {
 				errs() << "\t" << *def << "\n";
     		}
+    		*/
+    		
+    		errs() << "\nOUT:\n";
+    		for (std::set<std::string>::iterator o = outMap[currentBlock].begin(), oEnd = outMap[currentBlock].end(); o != oEnd; o++) {
+				errs() << "  " << *o << "\n";
+    		}
+    		if (outMap[currentBlock].size() == 0) {
+    			errs() << "  None\n";
+    		}
     	}
-    
-    	/*
-		for (Function::iterator currentBlock = F.begin(), endBlock = F.end(); currentBlock != endBlock; currentBlock++) {
-			
-			errs() << "\nBASIC BLOCK " << currentBlock->getName() << "\n";
-			
-			for (succ_iterator iterator = succ_begin(currentBlock), endSucc = succ_end(currentBlock); iterator != endSucc; iterator++) {
-			  	BasicBlock *succ = *iterator;
-			  	
-			  	errs() << "\nSUCCESSORS " << succ->getName() << "\n";
-			}
-			
-		}
-		*/
 		
 		//Function does not modify anything
 		return false;
--- llvm/cs565/tests/test4.c
+++ llvm/cs565/tests/test4.c
@@ -2,9 +2,9 @@
 int main() {
 	int a = 1;
 	int b = 2;
-	int e;
 	int c;
 	int d;
+	int e;
 L0: 
 	c = a + b; 
 	d = c - a;
@@ -16,13 +16,13 @@ L2:
  	b = a + b;
  	e = c - a;
  	if (e == 0) goto L0;
+ 	
  	a = b + d;
  	b = a - d;
  	goto L4;
 L3: 
  	d = a + b; 
  	e = e + 1;
- 	goto L3;
 L4: 
  	return 0;
 }
